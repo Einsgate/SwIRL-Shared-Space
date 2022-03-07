e895bc9590e4c933b940996c76868ffd20e2f541
Junjie Wang <einsgate@gmail.com>
2022-03-07 23:33:18 +0000

    Build up first demo (without static resources)

--- reservation/views.py
+++ reservation/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    t = loader.get_template('index.html')
+    return HttpResponse(t.render())
