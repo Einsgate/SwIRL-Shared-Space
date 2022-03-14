6f7d7252d0c709162a7765455a2f733a9bc75bb8
Junjie Wang <einsgate@gmail.com>
2022-03-14 22:14:30 +0000

    Change database default user

--- sharedspace/settings.py
+++ sharedspace/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'sharedspace',
-        'USER': 'jjwang',
-        'PASSWORD': '123456',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
