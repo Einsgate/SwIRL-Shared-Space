158392276f386b0641b5260d4e1ddbd72287fc1e
Junjie Wang <einsgate@gmail.com>
2022-03-14 08:38:27 +0000

    update settings

--- sharedspace/settings.py
+++ sharedspace/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'sharedspace',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
+        'USER': 'jjwang',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
