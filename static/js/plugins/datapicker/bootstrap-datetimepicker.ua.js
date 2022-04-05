212e153f04d858ad872180c103db2b90c14a19a1
DuodenumHou <hibernatehou@tamu.edu>
2022-04-05 18:32:06 -0500

    finish datatime-picker at training_list.html

---
+++ static/js/plugins/datapicker/bootstrap-datetimepicker.ua.js
@@ -0,0 +1,16 @@
+/**
+ * USA translation for bootstrap-datepicker
+ * 
+ */
+;(function($){
+	$.fn.datetimepicker.dates['zh-CN'] = {
+			days: ["Sunday", "Mondy", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+			daysShort: ["Sunday", "Mondy", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+			daysMin:  ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+			months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "October"],
+			monthsShort: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "October"],
+			today: "Today",
+			suffix: [],
+			meridiem: ["AM", "PM"]
+	};
+}(jQuery));
